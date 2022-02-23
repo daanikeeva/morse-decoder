76f32be864bcc5b1dc32cfd8d8ef8ac29e041006
Darya Anikeeva <daanikeeva@gmail.com>
2022-02-24 00:22:30 +0300

    feat: Morse code decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
+    const encodedLettersArr = []
+    let exprArr = expr.split('');
+          while (exprArr.length) {
+             encodedLettersArr.push(exprArr.slice(0,10).join(''));
+             exprArr.splice(0,10);
+           }
+           const dotDashLetter = []
+   encodedLettersArr.forEach(function decodeInDotDash(elem) {
+ let dotDash = '';
+       for (let i = 0; i <= elem.length+1; i += 2) {
+          if (elem.slice(i, i+2) === '11') {
+             dotDash += '-'
+          }
+          else if (elem.slice(i, i+2) === '10') {
+             dotDash += '.'
+          }
+          else if (elem.slice(i, i+2) === '**') {
+             dotDash += ' '
+             break
+          }
+          }
+          dotDashLetter.push(dotDash)
+    })
+    console.log(dotDashLetter)
+    resArr = []
+    dotDashLetter.forEach(elem => {
+       if (elem === ' ') {
+          resArr.push(elem)
+       }
+       else {
+          for (key in MORSE_TABLE) {
+             if (elem === key) {
+                resArr.push(MORSE_TABLE[key])
+             }
+          }
+       }
+    })
+    return resArr.join('')
+ }
+  module.exports = {
     decode
 }
